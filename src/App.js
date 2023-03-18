fb601f5a8427eba973ab4c063f38663967ccf32f
Matt McGavern <matt.mcgavern@gmail.com>
2023-03-18 01:57:58 -0500

    basic file and folder structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React from 'react';
+import {
+  Blog,
+  Features,
+  Footer,
+  Header,
+  Possibility,
+  WhatGPT3,
+} from './containers';
+import { CTA, Brand, Navbar } from './components';
 
 const App = (props) => {
   return (
     <div>
       <h1>GPT-3</h1>
     </div>
-  )
+  );
 };
 
 export default App;
